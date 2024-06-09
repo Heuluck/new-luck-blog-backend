a025688eb795005948e2cb28f269eb849f302e54
Heuluck Lu <89375068+Heuluck@users.noreply.github.com>
2024-06-09 23:54:48 +0800

    feat: login and register

--- src/routes/auth/AuthRouter.ts
+++ src/routes/auth/AuthRouter.ts
@@ -1,13 +1,6 @@
 import { Router } from "express";
 import jetValidator from "jet-validator";
 import Paths from "../../common/Paths";
-import User from "@src/models/User";
-import UserRoutes from "../user/UserRoutes";
-import { IRes } from "../types/express/misc";
-import { IReq } from "../types/types";
-import HttpStatusCodes from "@src/common/HttpStatusCodes";
-import { signJWT, verifyJWT } from "@src/util/jwt";
-import CryptoJS from "crypto-js";
 import AuthRoutes from "./AuthRoutes";
 
 // **** Variables **** //
@@ -18,11 +11,11 @@ const validate = jetValidator();
 
 const authRouter = Router();
 
-authRouter.get(Paths.Auth.REST, (_: IReq, res: IRes) => {
-    signJWT({ name: "test" }, "1d").then((token) => {
-        return res.status(HttpStatusCodes.OK).json({ token });
-    });
-});
+authRouter.post(
+    Paths.Auth.REST,
+    validate(["name", "string", "body"], ["password", "string", "body"]),
+    AuthRoutes.Login
+);
 
 authRouter.post(
     Paths.Auth.Register,
