15032fcf41dce6a628b6bc4a8e0eca6046ba168d
Heuluck Lu <89375068+Heuluck@users.noreply.github.com>
2024-06-10 00:19:52 +0800

    feat: add cors

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
-import Paths from '../common/Paths';
+import Paths from "../common/Paths";
 import pingRouter from "./ping/ping";
 import blogRouter from "./blog/BlogRouter";
 import authRouter from "./auth/AuthRouter";
+import cors from "cors";
 
 const apiRouter = Router();
 
+apiRouter.use(cors());
 
 apiRouter.use(Paths.Ping.Base, pingRouter);
 apiRouter.use(Paths.Blog.Base, blogRouter);
